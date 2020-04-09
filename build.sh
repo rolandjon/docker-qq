2ddd1a47ed5530abe7865f0cebaabb8ae291ca06
rland <rlandjon@gmail.com>
2020-04-09 10:24:02 +0800

    update url

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t rolandjon/qq:office -f office/Dockerfile .
-docker build -t rolandjon/qq:im -f im/Dockerfile .
-docker build -t rolandjon/qq:light -t bestwu/qq -f im.light/Dockerfile .
-docker build -t rolandjon/qq:eim -f eim/Dockerfile .
+docker build -t rlandj/qq:office -f office/Dockerfile .
+docker build -t rlandj/qq:im -f im/Dockerfile .
+docker build -t rlandj/qq:light -t bestwu/qq -f im.light/Dockerfile .
+docker build -t rlandj/qq:eim -f eim/Dockerfile .
