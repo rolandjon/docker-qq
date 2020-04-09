bb2d80c62fc0128f8eb24fabd9741c6160aa490c
rland <rlandjon@gmail.com>
2020-04-09 10:30:00 +0800

    rm unused dockerfile

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t rlandj/qq:office -f office/Dockerfile .
-docker build -t rlandj/qq:im -f im/Dockerfile .
-docker build -t rlandj/qq:light -t bestwu/qq -f im.light/Dockerfile .
-docker build -t rlandj/qq:eim -f eim/Dockerfile .
